f51b76a667feb9de4209527a226f5ee48b168b09
NicolasRyhan <nicolasryhan81@gmail.com>
2025-07-09 11:47:10 -0300

    removendo o arquivo banco.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const dummyTransaction = [
+    {id: 1, name: 'Bolo de Brigadeiro', amaount: -20},
+    {id: 2, name: 'Salário', amaount: 300},
+    {id: 3, name: 'Bolo de Brigadeiro', amaount: -10},
+    {id: 4, name: 'Bolo de Brigadeiro', amaount: 150}
+];
+
+const addTransactionIntoDoom => {
+
+
+    <li class="minus">
+        Salário <span>-$400</span><button class="delete-btn">x</button>
+    </li>
+};
